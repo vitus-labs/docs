42d28fb550fa41a75f8b049a1015591f1c3a5c54
Vit Bokisch <vit@bokisch.cz>
2021-03-22 16:09:34 +0100

    table update

--- src/components/base/Table/Table.tsx
+++ src/components/base/Table/Table.tsx
@@ -1,3 +1,10 @@
 import { element } from '~/components/core'
 
-export default element.config({ name: 'base/Table/Table', component: 'table' })
+export default element
+  .config({ name: 'base/Table/Table', component: 'table' })
+  .styles(
+    (css) =>
+      css`
+        border-collapse: collapse;
+      `
+  )
