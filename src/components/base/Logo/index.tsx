d056f6e598605dcfd6d074e00b2e50eec6bd737a
Vit Bokisch <vit@bokisch.cz>
2021-03-22 15:45:47 +0100

    change logo color

--- src/components/base/Logo/index.tsx
+++ src/components/base/Logo/index.tsx
@@ -2,14 +2,18 @@ import { VFC } from 'react'
 import { element } from '~/components/core'
 import Link from '~/components/base/Link'
 
-const Logo = element.attrs({
-  tag: 'span',
-  contentAlignX: 'left',
-  contentAlignY: 'center',
-  dangerouslySetInnerHTML: {
-    __html: require(`~/assets/logo/vitus-labs.svg?include`),
-  },
-})
+const Logo = element
+  .attrs({
+    tag: 'span',
+    contentAlignX: 'left',
+    contentAlignY: 'center',
+    dangerouslySetInnerHTML: {
+      __html: require(`~/assets/logo/vitus-labs.svg?include`),
+    },
+  })
+  .theme((t) => ({
+    color: t.color.primary.base,
+  }))
 
 const component: VFC = () => (
   <Link href="/">
