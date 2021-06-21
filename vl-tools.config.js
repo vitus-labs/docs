ab7d750858aa6dcf4ef446c90cf774febc03c6e7
Vit Bokisch <vit@bokisch.cz>
2021-06-21 21:42:47 +0200

    add favicon pkg

---
+++ vl-tools.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  favicon: {
+    icons: [
+      {
+        input: 'src/assets/favicon/light.svg',
+        output: 'public/favicon',
+        path: 'light',
+      },
+    ],
+    path: `/favicon/`,
+  },
+}
