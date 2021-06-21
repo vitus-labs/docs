57b1bae208ec4aa00938a0991354e698abc48ba6
Vit Bokisch <vit@bokisch.cz>
2021-06-21 21:54:40 +0200

    add package for generating favicons

--- vl-tools.config.js
+++ vl-tools.config.js
@@ -1,12 +1,22 @@
 module.exports = {
   favicon: {
+    appName: 'Vitus Labs',
+    appShortName: 'Vitus Labs',
+    appDescription: 'Build apps like a PRO',
+    developerName: 'Vit Bokisch <vit@bokisch.cz>',
+    developerURL: 'https://vitus-labs.com',
+    path: `/favicon`,
     icons: [
       {
         input: 'src/assets/favicon/light.svg',
-        output: 'public/favicon',
+        output: 'public/favicon/light',
         path: 'light',
       },
+      {
+        input: 'src/assets/favicon/dark.svg',
+        output: 'public/favicon/dark',
+        path: 'dark',
+      },
     ],
-    path: `/favicon/`,
   },
 }
