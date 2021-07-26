a0053069cd53a8bafc21b9796a76e2ccf9915220
Vit Bokisch <vit@bokisch.cz>
2021-07-26 12:49:05 +0200

    List items add spacing

--- src/components/base/TextList/Item.ts
+++ src/components/base/TextList/Item.ts
@@ -1,5 +1,10 @@
 import { text } from '~/components/core'
 
-export default text.config({ name: 'base/TextList/Item' }).attrs({
-  tag: 'li',
-})
+export default text
+  .config({ name: 'base/TextList/Item' })
+  .attrs({
+    tag: 'li',
+  })
+  .theme((t) => ({
+    marginLeft: t.spacing.large,
+  }))
